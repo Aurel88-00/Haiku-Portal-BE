d9f7994ad88daf4505d821061f11633fa5a04dc2
Aurel Mirashi <aurelmirashidmc@gmail.com>
2025-06-26 18:19:31 +0200

    Updated modules

--- src/Modules/author/author.module.ts
+++ src/Modules/author/author.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthorController } from 'src/Controllers/author/author.controller';
+import { AuthorSchema } from 'src/lib/schemas/author.schema';
+import { HaikuSchema } from 'src/lib/schemas/haiku.schema';
 import { AuthorService } from 'src/Services/author/author.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Haiku',
+        schema: HaikuSchema,
+      },
+      {
+        name: 'Author',
+        schema: AuthorSchema,
+      },
+    ]),
+  ],
   controllers: [AuthorController],
   providers: [AuthorService],
 })
--- src/Modules/haiku/haiku.module.ts
+++ src/Modules/haiku/haiku.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { HaikuController } from 'src/Controllers/haiku/haiku.controller';
+import { AuthorSchema } from 'src/lib/schemas/author.schema';
+import { HaikuSchema } from 'src/lib/schemas/haiku.schema';
 import { HaikuService } from 'src/Services/haiku/haiku.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Haiku',
+        schema: HaikuSchema,
+      },
+      {
+        name: 'Author',
+        schema: AuthorSchema,
+      },
+    ]),
+  ],
   controllers: [HaikuController],
   providers: [HaikuService],
 })
